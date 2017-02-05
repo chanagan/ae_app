1e31196ca49546beb6e30fc7b6f94e71b9a26254
Chris Hanagan <chris.hanagan@gmail.com>
2017-02-04 20:57:51 -0500

    Basic grid is working

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,82 @@
+var execDate;
+
+function planLoadSuccess(data, textStatus) {
+    // so now make a grid out of the JSON object we came in with
+
+    //    var flt_json = JSON.parse(data);
+    var flt_json = data.flight;
+    var t1 = 1234;
+
+    // 1 and 2 - not using block separators (US,RS,GS), so
+    // the thing is already parsed into flight and mission fields
+
+    var gridBody;
+    gridBody = $("#gridTblBdy");
+    gridBody.empty();
+
+    $.each(flt_json, function(idx, entry) {
+        var t1;
+        var fltRow;
+        fltRow = entry;
+        t1 = 1234;
+    })
+}
+
+function planLoad() {
+    if (execDate.length < 3) {
+        return;
+    }
+
+    var tStr = '124';
+    var showDebug = false;
+    tStr = 'abcd';
+
+    $.ajax({
+        //        cache: false,
+        type: 'GET',
+        dataType: 'json',
+        //        url: "/EDB/missions/air_exec/getMissionGrid.pl",
+        //        url: "http://localhost:3001/get_flights/" + '03-22-2016',
+        url: "http://localhost:3001/get_flights/" + execDate,
+        processData: true,
+        //        data: "03-22-2016",
+        success: function(data, textStatus, zz) {
+            // if (showDebug) {
+            //     $('#plTrail').html(data);
+            //     showTrail();
+            // }
+            planLoadSuccess(data, textStatus);
+            //                planLoadSuccess("not a chance", textStatus);
+            var tStr = '1234';
+            tStr = 'abcd';
+        },
+        error: function(reqObj, textStatus, errorThrown) {
+            statusLine = 'Air Plan Load: . . ' + execDate + ' . . ' + reqObj.statusText;
+            alert(statusLine);
+        },
+        complete: function(reqObj, textStatus) {
+            statusLine = 'Air Plan Load: . . ' + execDate + ' . . ' + reqObj.statusText;
+            //            document.parentWindow.status = statusLine;
+
+            // if (showGrid == true) {
+            //     $('#plTrail').html(reqObj.responseText);
+            //     showTrail();
+            // }
+
+        }
+    });
+}
+
+function firstLoad() {
+    alert("show plan for " + execDate);
+    planLoad();
+
+}
+
 $(document)
     .ready(function() {
-        var rdt = $('#hdrDateA').text();
-        alert('test:' + rdt);
-
-        console.log("ready!");
+        execDate = $('#hdrDateA').text();
+        firstLoad();
+        // alert('test:' + execDate);
+        // console.log("ready!");
     });
